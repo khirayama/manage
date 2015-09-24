33a0c69ed47ddd6e2f1d6256112c92b2bb3460ed
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-09-25 07:41:54 +0900

    [wip] refactor Dispatcher.js

--- src/scripts/libs/Dispatcher.js
+++ src/scripts/libs/Dispatcher.js
@@ -1,22 +1,41 @@
 export default class Dispatcher {
-  on(event, callback) {
-    this._events = this._events || {};
+  constructor() {
+    this._events = {};
+  }
+
+  addListener(event, callback) {
     this._events[event] = this._events[event] || [];
     this._events[event].push(callback);
   }
 
-  off(event, callback) {
-    this._events = this._events || {};
+  /* Alias of addListener */
+  on(event, callback) {
+    this.addListener(event, callback);
+  }
+
+  removeListener(event, callback) { // alias
     if (event in this._events === false) return;
     this._events[event].splice(this._events[event].indexOf(callback), 1);
   }
 
-  dispatch(event, data) {
-    this._events = this._events || {};
+  /* Alias of removeListener */
+  off(event, callback) {
+    this.removeListener(event, callback);
+  }
+
+  emit(event, payload) {
     if (event in this._events === false) return;
     for (let i = 0; i < this._events[event].length; i++) {
-      this._events[event][i].apply(this, [data]);
+      this._events[event][i].apply(this, [payload]);
     }
+  }
+
+  /* Alias of emit */
+  dispatch(event, payload) {
+    this.emit(event, payload);
+  }
+
+  once(event, callback) {
   }
 }
 
