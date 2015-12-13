7f582f169f5f8ec9d99074cafe8e36be0c55d850
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-12-13 16:37:31 +0900

    Change stack

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ManageApp from './components/ManageApp';
 
-(() => {
-  ReactDOM.render(<ManageApp />, document.querySelector('#manage-app'));
-})();
+export default class App {
+  constructor() {
+    ReactDOM.render(<ManageApp />, document.querySelector('#manage-app'));
+  }
+}
