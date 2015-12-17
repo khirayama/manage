0762f4de9d0e1484ff4271e80308330f7b0f5cb6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-12-18 07:25:41 +0900

    Add test cases

--- test/storage/micro-storage.js
+++ test/storage/micro-storage.js
@@ -1,5 +1,7 @@
 import assert from 'power-assert';
 
+console.log(assert);
+
 import MicroStorage from '../../src/scripts/storages/micro-storage';
 
 
@@ -34,33 +36,6 @@ describe('MicroStorage', () => {
       assert(false === todo.completed);
       assert(undefined !== todo.id);
     });
-    it('an item with text', () => {
-      todoStorage.create({
-        text: 'Hello World',
-      });
-
-      const todos = todoStorage.all();
-      const todo = todos[0];
-
-      assert(1 === todos.length);
-      assert('Hello World' === todo.text);
-      assert(false === todo.completed);
-      assert(undefined !== todo.id);
-    });
-    it('an item with completed', () => {
-      todoStorage.create({
-        text: 'Hello World',
-        completed: true,
-      });
-
-      const todos = todoStorage.all();
-      const todo = todos[0];
-
-      assert(1 === todos.length);
-      assert('Hello World' === todo.text);
-      assert(true === todo.completed);
-      assert(undefined !== todo.id);
-    });
   });
 
   describe('update', () => {
@@ -77,12 +52,76 @@ describe('MicroStorage', () => {
       todo = todos[0];
     });
 
+    it('all', () => {
+      todo = todoStorage.update(todo.id, {
+        text: 'Hello New World',
+        completed: true,
+      });
+
+      assert(todo.text === 'Hello New World');
+      assert(todo.completed === true);
+    });
+
     it('text', () => {
       todo = todoStorage.update(todo.id, {
         text: 'Hello New World',
       });
 
-      assert('Hello New World' === todo.text);
+      assert(todo.text === 'Hello New World');
+    });
+  });
+
+  describe('destroy', () => {
+    let todos;
+    let todo;
+
+    beforeEach(() => {
+      todoStorage.create({
+        text: 'Hello World',
+        completed: true,
+      });
+
+      todos = todoStorage.all();
+      todo = todos[0];
+    });
+
+    it('an item', () => {
+      todoStorage.destroy(todo.id);
+
+      todos = todoStorage.all();
+      assert(todos.length === 0);
+    });
+  });
+
+  describe('drop', () => {
+    let todos;
+    let todo;
+
+    beforeEach(() => {
+      todoStorage.create({
+        text: 'Hello World',
+        completed: true,
+      });
+      todoStorage.create({
+        text: 'Hello World 2',
+        completed: true,
+      });
+      todoStorage.create({
+        text: 'Hello World 3',
+        completed: true,
+      });
+
+      todos = todoStorage.all();
+      todo = todos[0];
+    });
+
+    it('all item', () => {
+      assert(todos.length === 3);
+
+      todoStorage.drop();
+
+      todos = todoStorage.all();
+      assert(todos.length === 0);
     });
   });
 });
