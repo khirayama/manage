e66b44ece16765d903b49f59a9aa9d98b0e1ee83
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-12-21 07:54:38 +0900

    Refactore todo-action-creators

--- src/scripts/actions/todo-action-creators.js
+++ src/scripts/actions/todo-action-creators.js
@@ -1,17 +1,47 @@
-import AppDispatcher from '../dispatchers/AppDispatcher';
-import { todoEvents } from '../constants/constants';
+import appDispatcher from '../dispatchers/app-dispatcher';
+import todoStorage from '../storages/todo-storage';
 
-class TodoActions {
-  create(entity) {
-    AppDispatcher.emit(todoEvents.CREATE, { entity: entity });
-  }
-
-  update(id, updates) {
-    AppDispatcher.emit(todoEvents.UPDATE, { id: id, updates: updates });
-  }
-
-  destroy(id) {
-    AppDispatcher.emit(todoEvents.DESTROY, { id: id });
-  }
+const types = {
+  CREATE_TODO: 'CREATE_TODO'
+  COMPLETE_TODO: 'COMPLETE_TODO',
+  EDIT_TODO: 'EDIT_TODO',
+  DELETE_TODO: 'DELETE_TODO',
 }
-export default new TodoActions();
+
+export function createTodo(text, categoryId) {
+  const entity = todoStorage.create({
+    text,
+    categoryId,
+  });
+
+  appDispatcher.emit(types.CREATE_TODO, entity);
+
+  return entity;
+}
+
+export function completeTodo(id) {
+  const todo = todoStorage.get(id);
+  const entity = todoStorage.update(todo.id, {
+    completed: !todo.completed,
+  });
+
+  appDispatcher.emit(types.COMPLETE_TODO, entity);
+
+  return entity;
+}
+
+export function editTodo(id, text) {
+  const todo = todoStorage.get(id);
+  const entity = todoStorage.update(todo.id, { text });
+
+  appDispatcher.emit(types.EDIT_TODO, entity);
+
+  return entity;
+}
+
+export function deleteTodo(id) {
+  todoStorage.destroy(id);
+  appDispatcher.emit(types.DELETE_TODO, { id });
+
+  return null;
+}
