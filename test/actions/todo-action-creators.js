e8799d8c4a914b1e49b647db34f5233cce0be003
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-12-23 07:32:52 +0900

    Cover todo-action-creators/editTodo in test

--- test/actions/todo-action-creators.js
+++ test/actions/todo-action-creators.js
@@ -6,11 +6,17 @@ import {
   editTodo,
   deleteTodo,
 } from '../../src/scripts/actions/todo-action-creators';
+import todoStorage from '../../src/scripts/storages/todo-storage';
 import appDispatcher from '../../src/scripts/dispatchers/app-dispatcher';
 import { actionTypes as types } from '../../src/scripts/constants/constants';
 
 
 describe('TodoActionCreators', () => {
+  beforeEach(() => {
+    todoStorage.drop();
+    appDispatcher._listeners = {};
+  });
+
   describe('createTodo', () => {
     it('an item', (done) => {
       appDispatcher.on(types.CREATE_TODO, (todo) => {
@@ -19,6 +25,22 @@ describe('TodoActionCreators', () => {
         done();
       });
       createTodo('Hello World');
+    });
+  });
+
+  describe('editTodo', () => {
+    it('an item', (done) => {
+      appDispatcher.on(types.EDIT_TODO, (todo) => {
+        assert(todo.id !== undefined);
+        assert(todo.text === 'Hello New World');
+        done();
+      });
+      createTodo('Hello World');
+
+      let todos = todoStorage.all();
+      let todo_ = todos[0];
+
+      editTodo(todo_.id, 'Hello New World');
     });
   });
 });
