e3476560a308276c7d5322a2a436037182cc0411
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-12-26 10:33:46 +0900

    All green to lint

--- src/scripts/components/todo-list-item.jsx
+++ src/scripts/components/todo-list-item.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 export default class TodoItem extends Component {
   render() {
     const todo = this.props.todo;
-    let textComponent;
 
     return (
       <li key={todo.id} >
