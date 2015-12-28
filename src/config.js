9a5adb0ad7d0d3d3efc4f06485ac24162a11335b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-12-29 07:47:09 +0900

    Create config file and update logger

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  env: 'development',
+  logLevels: [0, 1, 2, 3],
+};
+
+export default config;
--- src/scripts/utils/logger.js
+++ src/scripts/utils/logger.js
@@ -1,19 +1,34 @@
 /* eslint "no-console": 0 */
 
+import config from '../../config';
+
+const logLevels = {
+  DEBUG: 0,
+  INFO: 0,
+  TRACE: 1,
+  WARN: 2,
+  ERROR: 3,
+};
+
 const logger = {
+  debug: (...args) => {
+    if (config.logLevels.indexOf(logLevels.DEBUG) === -1) return;
+    console.log(...args);
+  },
   info: (...args) => {
+    if (config.logLevels.indexOf(logLevels.INFO) === -1) return;
     console.log(...args);
   },
   trace: (...args) => {
-    console.log(...args);
-  },
-  debug: (...args) => {
-    console.log(...args);
+    if (config.logLevels.indexOf(logLevels.TRACE) === -1) return;
+    console.trace(...args);
   },
   warn: (...args) => {
+    if (config.logLevels.indexOf(logLevels.WARN) === -1) return;
     console.warn(...args);
   },
   error: (...args) => {
+    if (config.logLevels.indexOf(logLevels.ERROR) === -1) return;
     console.error(...args);
   },
 };
