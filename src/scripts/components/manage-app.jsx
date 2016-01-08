7229cd06953690329941608c34658bdf483e2ed8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-01-09 08:50:49 +0900

    Update page structure

--- src/scripts/components/manage-app.jsx
+++ src/scripts/components/manage-app.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 import { pages } from '../constants/constants';
-import { changePage, backPage } from '../actions/app-action-creators';
-import Header from './header';
-import TodoList from './todo-list';
-import TodoCategoryList from './todo-category-list';
+import TodosPage from './todos-page';
+import MenuPage from './menu-page';
+import TodoCategoriesPage from './todo-categories-page';
+
 
 export default class ManageApp extends Component {
   constructor(props) {
@@ -25,14 +25,6 @@ export default class ManageApp extends Component {
     this.props.appStore.removeChangeListener(this.updateState);
   }
 
-  onClickLink(page) {
-    changePage(page);
-  }
-
-  onClickBack() {
-    backPage();
-  }
-
   _updateState() {
     this.setState({
       appStore: this.props.appStore,
@@ -43,74 +35,28 @@ export default class ManageApp extends Component {
     document.title = `${title} | Manage`;
   }
 
-  createTodosPage() {
-    const page = this.state.appStore.getPage();
-    const todos = this.props.appStore.todoStore.getTodos();
-
-    this._changeTitle('Todos');
-
-    const todoListElements = todos.map((todoCategory) => {
-      return <TodoList key={todoCategory.categoryId} todoCategory={todoCategory} />;
-    });
-
-    return (
-      <section className="page todos-page">
-        <Header page={ page } />
-        <section className="page-content">
-          { todoListElements }
-        </section>
-      </section>
-    );
-  }
-
-  createMenuPage() {
-    const page = this.state.appStore.getPage();
-
-    this._changeTitle('Menu');
-
-    return (
-      <section className="page menu-page">
-        <section className="page-content">
-          <div className="page-back-button" onClick={ this.onClickBack.bind(this) }>←</div>
-          <ul className="menu-list">
-            <li className="menu-list-item" onClick={ this.onClickLink.bind(this, pages.TODOS) }>Show todos</li>
-            <li className="menu-list-item" onClick={ this.onClickLink.bind(this, pages.TODO_CATEGORIES) }>Show todo categories</li>
-          </ul>
-        </section>
-        <Header page={ page } position="bottom" />
-      </section>
-    );
-  }
-
-  createTodoCategoriesPage() {
-    const page = this.state.appStore.getPage();
-    const todoCategories = this.props.appStore.todoCategoryStore.getTodoCategories();
-
-    this._changeTitle('Todo categories');
-
-    return (
-      <section className="page todo-categories-page">
-        <section className="page-content">
-          <div className="page-back-button" onClick={ this.onClickBack.bind(this) }>←</div>
-          <TodoCategoryList todoCategories={todoCategories} />
-        </section>
-        <Header page={ page } position="bottom" />
-      </section>
-    );
-  }
-
   render() {
     const page = this.state.appStore.getPage();
 
     switch (page) {
       case (pages.TODOS):
-        return this.createTodosPage();
+        const todos = this.state.appStore.todoStore.getTodos();
+
+        this._changeTitle('Todos');
+
+        return <TodosPage page={page} todos={todos} />;
       case (pages.MENU):
-        return this.createMenuPage();
+        this._changeTitle('Menu');
+
+        return <MenuPage page={page} />;
       case (pages.TODO_CATEGORIES):
-        return this.createTodoCategoriesPage();
+        const todoCategories = this.props.appStore.todoCategoryStore.getTodoCategories();
+
+        this._changeTitle('Todo categories');
+
+        return <TodoCategoriesPage page={page} todoCategories={todoCategories} />;
       default:
-        return this.createTodosPage();
+        return <div>404</div>;
     }
   }
 }
