ebabec14b43e2749477619250be9c3efc9889f7c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-05-26 13:07:29 +0900

    Integrate todo-list & todo-list-item files

--- src/scripts/components/todo-list.jsx
+++ src/scripts/components/todo-list.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import TodoListItem from './todo-list-item';
+import classNames from 'classnames';
 
 import { pages } from '../constants/constants';
 import { changePage } from '../actions/app-action-creators';
-import { createTodo } from '../actions/todo-action-creators';
+import {
+  createTodo,
+  completeTodo,
+  editTodo,
+  updateTodo,
+  deleteTodo,
+} from '../actions/todo-action-creators';
+import { keyCodes } from '../constants/constants';
 
 
-const propTypes = {
+const todoListPropTypes = {
   todoCategory: React.PropTypes.object,
   setCurrentOrder: React.PropTypes.func.isRequired,
   setNewOrder: React.PropTypes.func.isRequired,
   moveTodo: React.PropTypes.func.isRequired,
+};
+
+const todoListItemPropTypes = {
+  todo: React.PropTypes.object.isRequired,
+  setCurrentOrder: React.PropTypes.func,
+  setNewOrder: React.PropTypes.func,
+  moveTodo: React.PropTypes.func,
 };
 
 export default class TodoList extends Component {
@@ -94,4 +108,171 @@ export default class TodoList extends Component {
   }
 }
 
-TodoList.propTypes = propTypes;
+class TodoListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.todo.text,
+    };
+
+    this.onClickLabel = this.onClickLabel.bind(this);
+    this.onClickDoneButton = this.onClickDoneButton.bind(this);
+    this.onClickDeleteButton = this.onClickDeleteButton.bind(this);
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onDragEnter = this.onDragEnter.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
+    this.onChangeInput = this.onChangeInput.bind(this);
+    this.onKeyDownInput = this.onKeyDownInput.bind(this);
+    this.onBlurInput = this.onBlurInput.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.todo.isEditing && this.props.todo.isEditing) {
+      this.selectInputValue();
+    }
+  }
+
+  onClickLabel() {
+    editTodo(this.props.todo.id);
+  }
+
+  onClickDoneButton() {
+    completeTodo(this.props.todo.id);
+  }
+
+  onClickDeleteButton() {
+    deleteTodo(this.props.todo.categoryId, this.props.todo.id);
+  }
+
+  onDragStart() {
+    const todo = this.props.todo;
+
+    this.props.setCurrentOrder(todo.categoryId, todo.order);
+  }
+
+  onDragEnter() {
+    const todo = this.props.todo;
+
+    this.props.setNewOrder(todo.categoryId, todo.order);
+  }
+
+  onDragEnd() {
+    this.props.moveTodo();
+  }
+
+  onChangeInput(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  onKeyDownInput(event) {
+    const keyCode = event.keyCode;
+    const shift = event.shiftKey;
+    const ctrl = event.ctrlKey || event.metaKey;
+
+    switch (true) {
+      case (keyCode === keyCodes.ENTER && !shift && !ctrl):
+        this.save();
+        break;
+      case (keyCode === keyCodes.ENTER && !shift && ctrl):
+        if (this.state.value === '') {
+          deleteTodo(this.props.todo.categoryId, this.props.todo.id);
+        }
+        createTodo('', this.props.todo.categoryId);
+        break;
+      case (keyCode === keyCodes.ESC && !shift && !ctrl):
+        this.save();
+        break;
+      case (keyCode === keyCodes.TAB && !shift && !ctrl):
+        event.preventDefault();
+        editTodo(this.props.todo.categoryId, this.props.todo.order + 1);
+        break;
+      case (keyCode === keyCodes.TAB && shift && !ctrl):
+        event.preventDefault();
+        editTodo(this.props.todo.categoryId, this.props.todo.order - 1);
+        break;
+      default:
+        break;
+    }
+  }
+
+  onBlurInput() {
+    this.save();
+  }
+
+  save() {
+    const todo = this.props.todo;
+    const text = this.state.value.trim();
+
+    if (text !== '') {
+      updateTodo(todo.id, text);
+    } else {
+      deleteTodo(todo.categoryId, todo.id);
+    }
+  }
+
+  selectInputValue() {
+    this.refs.input.select();
+  }
+
+  render() {
+    const todo = this.props.todo;
+    let itemContent;
+    let categoryList;
+
+    if (todo.isEditing) {
+      itemContent = (
+        <input
+          autoFocus
+          ref="input"
+          placeholder={ 'Add a todo' }
+          value={ this.state.value }
+          onChange={ this.onChangeInput }
+          onKeyDown={ this.onKeyDownInput }
+          onBlur={ this.onBlurInput }
+        />
+      );
+    } else {
+      const itemContentProps = {
+        draggable: true,
+        onClick: this.onClickLabel,
+        onDragStart: this.onDragStart,
+        onDragEnter: this.onDragEnter,
+        onDragEnd: this.onDragEnd,
+      };
+
+      if (todo.schedule) {
+        const schedule = todo.schedule;
+        itemContent = (
+          <label { ...itemContentProps } >
+            { todo.scheduleText }
+            <div className="todo-list-item-schedule">
+              {schedule.year}/{schedule.month}/{schedule.date}({schedule.shortDayName}.)
+            </div>
+          </label>
+        );
+      } else {
+        itemContent = <label { ...itemContentProps } >{ todo.text }</label>;
+      }
+    }
+
+    return (
+      <li
+        key={ todo.id }
+        className={ classNames('todo-list-item', { 'is-completed': todo.completed }) }
+      >
+        <div>
+          <div className="done-button" onClick={ this.onClickDoneButton }><span>D</span></div>
+          { itemContent }
+          <div className="delete-button" onClick={ this.onClickDeleteButton }><span>[D]</span></div>
+        </div>
+        { categoryList }
+      </li>
+    );
+  }
+}
+
+TodoList.propTypes = todoListPropTypes;
+TodoListItem.propTypes = todoListItemPropTypes;
