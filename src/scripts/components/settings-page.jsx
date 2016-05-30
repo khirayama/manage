66970692cddea6469a445f7b247b5e03ca9f5e82
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-05-30 10:29:37 +0900

    Update setting list

--- src/scripts/components/settings-page.jsx
+++ src/scripts/components/settings-page.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import config from '../../config';
 
 import PageBackButton from './page-back-button';
 
@@ -24,6 +25,27 @@ export default class SettingsPage extends Component {
                   <div className="list-item-text">Clear all data</div>
                 </li>
               </ul>
+            </section>
+            <ul className="list">
+              <li className="list-item">
+                <div className="list-item-text">Help</div>
+              </li>
+              <li className="list-item">
+                <div className="list-item-text">Policy</div>
+              </li>
+            </ul>
+            <section className="list">
+              <ul>
+                <li className="list-item">
+                  <div className="list-item-text">Sign Out</div>
+                </li>
+                <li className="list-item">
+                  <div className="list-item-text">Delete account</div>
+                </li>
+              </ul>
+              <footer>
+                <small>2015- © {config.name}</small>
+              </footer>
             </section>
           </div>
         </section>
